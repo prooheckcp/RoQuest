13d9b0481f99e4c6c4ba9558c829463f70d9b5b4
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2024-05-10 01:31:26 +0100

    Finish cleaning up lifecycles!

---
+++ src/ReplicatedStorage/LifeCycles/Client/AppleQuest.lua
@@ -0,0 +1,41 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local RoQuest = require(ReplicatedStorage.RoQuest).Client
+
+type QuestLifeCycle = RoQuest.QuestLifeCycle
+
+local QuestLifeCycle = RoQuest.QuestLifeCycle
+
+local AppleQuest = QuestLifeCycle {
+    Name = "AppleQuest",
+}
+
+function AppleQuest:FirstStart()
+    print("Apple First Start")
+end
+
+function AppleQuest:AllComplete()
+    print("Apple All Complete")
+end
+
+function AppleQuest:OnStart()
+    print("Apple On Start")
+end
+
+function AppleQuest:OnComplete()
+    print("Apple On Complete")
+end
+
+function AppleQuest:OnObjectiveChange(...)
+    print(...)
+end
+
+function AppleQuest:OnDeliver()
+    print("Apple On Deliver")
+end
+
+function AppleQuest:OnDestroy()
+    print("Apple On Destroy")
+end
+
+return AppleQuest
