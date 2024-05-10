ec4c1f8d98ec1a175e273ed2a450290f6b1aef6f
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2024-05-10 01:29:52 +0100

    Loading client lifecycles

--- RoQuest/Client/init.lua
+++ RoQuest/Client/init.lua
@@ -1,4 +1,5 @@
 local Players = game:GetService("Players")
+
 local Red = require(script.Parent.Vendor.Red).Client
 local Signal = require(script.Parent.Vendor.Signal)
 local QuestLifeCycle = require(script.Parent.Shared.Classes.QuestLifeCycle)
--- src/ServerScriptService/main.server.lua
+++ src/ServerScriptService/main.server.lua
@@ -2,26 +2,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local RoQuest = require(ReplicatedStorage.RoQuest).Server
 
-RoQuest:Init(RoQuest:LoadDirectory(ReplicatedStorage.Quests), RoQuest:LoadDirectory(ReplicatedStorage.LifeCycles.Server))
-
-RoQuest.OnStart():andThen(function()
-    RoQuest.OnQuestStarted:Connect(function(player: Player, questId: string)
-        --print("Quest Started:", player, questId)
-    end)    
-    --[[
-
-
-    RoQuest.OnQuestAvailable:Connect(function(player: Player, questId: string)
-        print("Quest Available: ", player, questId)
-    end)
-
-    RoQuest.OnQuestUnavailable:Connect(function(player: Player, questId: string)
-        print("Quest Unavailable: ", player, questId)
-    end)
-
-    RoQuest.OnQuestDelivered:Connect(function(player: Player, questId: string)
-        print("Available Quest: ", RoQuest:GetAvailableQuests(player))
-    end)        
-    ]]
-
-end)
+RoQuest:Init(
+    RoQuest:LoadDirectory(ReplicatedStorage.Quests), 
+    RoQuest:LoadDirectory(ReplicatedStorage.LifeCycles.Server)
+)
